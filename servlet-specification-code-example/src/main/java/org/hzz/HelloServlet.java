2129e177ac3e19e10029da18ddd527441d0dfce1
静默 <cau1403090523@gmail.com>
2021-12-05 05:13:46 +0800

    update

--- servlet-specification-code-example/src/main/java/org/hzz/HelloServlet.java
+++ servlet-specification-code-example/src/main/java/org/hzz/HelloServlet.java
@@ -10,5 +10,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Hello Servlet... ...");
+        resp.getWriter().println("Back,hello.");
+        resp.flushBuffer();
     }
 }
