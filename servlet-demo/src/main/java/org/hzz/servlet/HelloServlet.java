2295a4ff55fdc9534c48f00f7a0bad31fd36db60
静默 <cau1403090523@gmail.com>
2021-12-07 04:59:20 +0800

    tomcat 7 run success

---
+++ servlet-demo/src/main/java/org/hzz/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package org.hzz.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/helloServlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello Tomcat 7.0.109.");
+        writer.flush();
+        writer.close();
+    }
+}
