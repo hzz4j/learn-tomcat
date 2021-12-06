6423312eac35731275b97fbd057028596210f4fb
静默 <cau1403090523@gmail.com>
2021-12-07 05:17:04 +0800

    read browser info

--- network-demo/src/main/java/org/hzz/bio/ServerDemo.java
+++ network-demo/src/main/java/org/hzz/bio/ServerDemo.java
@@ -6,21 +6,24 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerDemo {
-    private static int PORT = 8082;
+    private static final int PORT = 8082;
+
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(PORT)){
-            System.out.println("Server start up at port: "+PORT);
-            while(true){
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server start up at port: " + PORT);
+            while (true) {
                 Socket socket = serverSocket.accept();
-                System.out.println("连接成功："+socket.getInetAddress().getHostAddress()
-                        +" : " + socket.getPort());
+                System.out.println("连接成功：" + socket.getInetAddress().getHostAddress()
+                        + " : " + socket.getPort());
 
                 System.in.read(); // 阻塞
                 System.out.println("开始读取RecvBuf的数据");
-                byte[] bytes = new byte[1024*1024];
+                int read = 0;
+                byte[] bytes = new byte[1024 * 1024];
                 InputStream inputStream = socket.getInputStream();
-                int read = inputStream.read(bytes);
-                System.out.println(new String(bytes,0,read));
+                while ((read = inputStream.read(bytes)) != -1) {
+                    System.out.println(new String(bytes, 0, read));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
